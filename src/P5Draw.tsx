f5327d63120114b33e751466747d31f33fe03239
bhaviksingh <bhaviksingh@gmail.com>
2020-12-09 13:14:19 +0530

    Static examples, including some conditional/parametric ones are working

--- src/P5Draw.tsx
+++ src/P5Draw.tsx
@@ -1,11 +1,15 @@
 import React from "react"
 import p5 from "p5"
+import { Axiom, Letter } from "@bvk/lsystem";
 
 interface myProps {
-  commandString: string,
+  commandString: string | Axiom,
   length?: number
   angle?: number
-  center?: number[]
+  center?: number[],
+  width?: number,
+  height?: number,
+  strokeWeight?: number
 }
 export default class P5Draw extends React.Component<myProps> {
   private p5Context: p5 | undefined;
@@ -18,50 +22,82 @@ export default class P5Draw extends React.Component<myProps> {
   }
   sketch = (p: p5) => {
     p.setup = () => {
-      let ctx = p.createCanvas(800, 800);
+      let ctx = p.createCanvas(this.props.width || 800, this.props.height || 800, p.WEBGL);
+      p.angleMode(p.DEGREES);
     };
     p.draw = () => {
-      let length = this.props.length ? this.props.length * p.height : 0.01 * p.height;
-      let angle = this.props.angle ? this.props.angle : Math.PI / 2;
+      p.translate(-p.width / 2, -p.height / 2);
       let center = this.props.center !== undefined ? [p.width * this.props.center[0], p.height * this.props.center[1]] : [0, 0];
-      let lString = this.props.commandString;
-      let steps = lString.length;
-
       p.push();
       p.background(255, 255, 255);
-      //p.ellipse(10, 10, 50, 50);
       p.translate(center[0], center[1]);
       p.noFill();
-      p.strokeWeight(0.8);
-      p.colorMode(p.HSB);
-
-      for (let i = 0; i < steps; i++) {
-        let char = lString[i];
-        //p.stroke(i % 255, 100, 100);
-        p.stroke(0, 0, 0);
-        switch (char) {
-          case "F":
-            p.line(0, 0, length, 0);
-            p.translate(length, 0);
-            break;
-          case "f":
-            p.translate(length, 0);
-            break;
-          case "+":
-            p.rotate(angle)
-            break;
-          case "-":
-            p.rotate(-angle)
-            break;
-          default:
-            console.log(char + " isn't turtle command");
-        }
+      p.stroke(0, 0, 0);
+      let sw = this.props.strokeWeight || p.width / 1000;
+      p.strokeWeight(sw);
+      if ((this.props.commandString[0] as Letter).symbol) {
+        this.drawAxiom(p, this.props.commandString as Axiom);
+      } else {
+        this.drawString(p, this.props.commandString as string);
       }
-      p.colorMode(p.RGB);
       p.noLoop();
       p.pop();
     };
   };
+  drawString = (p: p5, cS: string) => {
+    let length = this.props.length ? this.props.length * p.height : 0.01 * p.height;
+    let angle = this.props.angle ? this.props.angle : Math.PI / 2;
+    let lString = cS;
+    let steps = lString.length;
+
+    for (let i = 0; i < steps; i++) {
+      p.stroke(0, 0, 0);
+      let char = lString[i];
+      this.drawChar(p, char, length, angle)
+    }
+  }
+  drawAxiom = (p: p5, cS: Axiom) => {
+    let defaultLength = this.props.length ? this.props.length * p.height : 0.01 * p.height;
+    let defaultAngle = this.props.angle ? this.props.angle : Math.PI / 2;
+    let lString = cS;
+    let steps = lString.length;
+    for (let i = 0; i < steps; i++) {
+      let letter = lString[i];
+      let char = letter.symbol;
+      let param = letter.params && letter.params.length == 1 ? letter.params[0] + "" : undefined;
+      let val = param && !isNaN(parseFloat(param)) ? parseFloat(param) : undefined
+      this.drawChar(p, char, val || defaultLength, val || defaultAngle);
+    }
+  }
+
+  drawChar = (p: p5, char: string, l: number, a: number) => {
+    switch (char) {
+      case "F":
+        p.line(0, 0, l, 0);
+        p.translate(l, 0);
+        break;
+      case "f":
+        p.translate(l, 0);
+        break;
+      case "+":
+        p.rotate(a)
+        break;
+      case "-":
+        p.rotate(-a)
+        break;
+      case "[":
+        p.push()
+        break;
+      case "]":
+        p.pop();
+        break;
+      case "E":
+        p.ellipse(0, 0, l, l);
+        break;
+      default:
+        console.log(char + " isn't turtle command");
+    }
+  }
 
   render = () => {
     return (<div ref={this.containerRef} />)
--- src/staticExamples.tsx
+++ src/staticExamples.tsx
@@ -1,7 +1,6 @@
-import React from "react";
 import P5Draw from "./P5Draw";
-import lSystem from "@bvk/lsystem";
-import { parseAxiom, parseProductions } from "@bvk/lsystem";
+import LSystem from "@bvk/lsystem";
+
 
 interface LExample {
   name: string;
@@ -11,6 +10,8 @@ interface LExample {
   length: number;
   iterations: number;
   center?: number[]
+  asObj?: boolean
+  strokeWeight?: number
 }
 
 export default function staticExamples() {
@@ -19,33 +20,58 @@ export default function staticExamples() {
     {
       name: "Koch Curve",
       axiom: "F", productions: [" F:F+F--F+F"],
-      angle: Math.PI / 3, iterations: 5, length: 0.003,
+      angle: 60, iterations: 5, length: 0.003,
       center: [0.1, 0.4]
     },
     {
       name: "Koch Island",
-      axiom: "F-F-F-F", productions: [" F:F-F+F+FF-F-F+F"],
-      angle: Math.PI / 2, iterations: 4, length: 0.0021, center: [0.25, 0.75]
+      axiom: "F-F-F-F", productions: [" F:F-F+F+FF-F-F+F"], asObj: true,
+      angle: 90, iterations: 4, length: 0.0021, center: [0.25, 0.75], strokeWeight: 0.3
+    },
+    {
+      name: "Simple tree",
+      axiom: `-(90)FA(25)`,
+      productions: ["A(x): F[+(x)FA(0.9*x)][-(x)FA(0.9*x)]"], asObj: true,
+      angle: 22.5, iterations: 5, length: 0.07, center: [0.5, 0.9]
+    }, {
+      name: "Plants with flowers",
+      axiom: `-(90)A(8)`,
+      productions: ["A(x){x>1}:F[-B][+B]A(x-1)", "A(x){x<=1}: BE", "B: FF"], asObj: true,
+      angle: 30, iterations: 10, length: 0.05, center: [0.5, 0.9]
     }
   ]
   function drawExamples() {
     let examplesDOM: JSX.Element[] = [];
     examples.forEach((example) => {
-      let exampleInfo = (<div> {example.name} {example.axiom} {example.productions} {example.angle} {example.iterations} {example.length}</div>)
-      let lS = new lSystem(parseAxiom(example.axiom), parseProductions(example.productions), example.iterations);
-      let finalString = lS.iterate();
-      let examplePreview =
-        (<P5Draw commandString={finalString}
-          length={example.length} angle={example.angle} center={example.center} />)
+      let lS = new LSystem(example.axiom, example.productions, example.iterations);
+      let output = example.asObj ? lS.getIterationAsObject() : lS.getIterationAsString();
 
-      let exampleOutput = (
-        <div className='small'> {finalString} </div>
+      let egGraphic = (
+        < div style={{ width: 500, height: 500, backgroundColor: "rgb(250,250,250)" }}>
+          <P5Draw commandString={output}
+            length={example.length} angle={example.angle}
+            center={example.center} strokeWeight={example.strokeWeight}
+            width={500} height={500} />
+        </div >)
+
+      let egInfo = (
+        <div>
+          <span className="bold">{example.name} </span><br />
+          Axiom: {example.axiom} <br />
+          Productions : {example.productions} <br />
+          Iterations: {example.iterations}, Angle: {example.angle}, Length: {example.length}
+        </div>)
+      let egOutput = (
+        <div className='small' style={{ height: "40ch", overflowY: "scroll" }}>
+          <span className="bold"> LSystem output: <br /> </span>
+          {lS.getIterationAsString()}
+        </div>
       )
       examplesDOM.push(
-        <div style={{ display: "flex", flexDirection: "column", gap: "4px" }}>
-          {exampleInfo}
-          {examplePreview}
-          {exampleOutput}
+        <div style={{ display: "flex", flexDirection: "column", gap: "6px", borderRight: "1px solid gray", paddingRight: "12px" }}>
+          {egInfo}
+          {egGraphic}
+          {egOutput}
         </div>)
     });
     return (<div style={{ display: "flex", flexDirection: "row", gap: "12px" }}> {examplesDOM} </div>)
